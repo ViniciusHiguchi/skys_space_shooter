8812fb3b16264330587909295aa39433ebefcb70
Vinicius Higuchi <vinicius-higuchi@outlook.com>
2020-03-17 18:38:07 -0300

    Start
    added an improvised spaceship
    added core scripts
    added player movement and tilt on movement

---
+++ Assets/Scripts/PlayerActionScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sps.PlayerAction
+{
+
+public class PlayerActionScript : MonoBehaviour
+{
+
+    [SerializeField] private float speedDxy = 5f;
+
+    public void Movement(Vector3 speed)
+    {
+        Tilt(speed);
+        speed *= speedDxy * Time.deltaTime;
+        transform.localPosition = transform.position + speed;
+    }
+
+    public void Tilt(Vector3 tiltInfo)
+    {
+        Vector3 tiltTransform = new Vector3(0,0,0);
+        tiltTransform.y = -tiltInfo.x;
+        tiltTransform.x = tiltInfo.y;
+
+        transform.eulerAngles = tiltTransform * 5;
+    }
+
+}
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sps.PlayerAction;
+
+namespace Sps.PlayerControl
+{
+
+public class PlayerController : MonoBehaviour
+{
+    public Vector3 speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed = new Vector3(0,0,0);
+
+        if(Input.GetKey("up"))
+            speed.y += 1;
+        if(Input.GetKey("down"))
+            speed.y -= 1;
+        if(Input.GetKey("right"))
+            speed.x += 1;
+        if(Input.GetKey("left"))
+            speed.x -= 1;
+
+        
+        GetComponent<PlayerActionScript>().Movement(speed);
+    }
+}
+}
