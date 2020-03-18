60a26101e2579d9a6188a8ce37c257f25434656f
Vinicius Higuchi <vinicius-higuchi@outlook.com>
2020-03-17 21:36:06 -0300

    gitflow-feature-stash: feat1

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = new Vector3(0,0,100);
+    }
+    
+    private void OnCollisionEnter(Collision colided) 
+    {
+        Destroy(colided.collider.gameObject);
+        Destroy(this.gameObject);
+      
+    }
+    
+}
